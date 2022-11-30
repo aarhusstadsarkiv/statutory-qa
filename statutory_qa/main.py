581a5bca908c076e476037ca0b09919eab8386ff
Claus Juhl Knudsen <cjk@aarhus.dk>
2022-11-30 22:09:05 +0100

    Version 2, alpha

--- statutory_qa/main.py
+++ statutory_qa/main.py
@@ -2,10 +2,9 @@ import xml.etree.ElementTree as ET
 import shutil
 import argparse
 from pathlib import Path
-import os
 
 
-ILLIGAL_NAMES = [
+ILLEGAL_NAMES = [
     "con",
     "prn",
     "aux",
@@ -32,7 +31,7 @@ ILLIGAL_NAMES = [
 
 
 def get_version() -> str:
-    version: str = "Ukendt version"
+    version = "Ukendt version"
     with open(Path(__file__).absolute().parent.parent / "pyproject.toml") as i:
         for line in i.readlines():
             if line.startswith("version"):
@@ -40,18 +39,53 @@ def get_version() -> str:
     return version
 
 
-def parse_docIndex_xml(
-    xml_file: Path, max_examples: int
-) -> dict[str, list[Path]]:
-    """Returns a dictionary containing suffixes as keys and lists
-    of relative paths to be copied as values.
+def parse_fileIndex_xml(xml_file: Path) -> dict[str, str]:
+    """Returns a dictionary containing the relative_paths and checksums
+    of ALL files in the 'Documents' folder of the AVID.
 
     Params:
-        xml_file: Path to a valid and wellformed docIndex.xml file.
+        xml_file: Path to a valid and wellformed fileIndex.xml
+
+    Returns:
+        A dict relative_paths and checksums of all the files in the
+          'Documents' folder of the AVID.
+    """
+    checksums: dict[str, str] = {}
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
+    ns = ".//{http://www.sa.dk/xmlns/diark/1.0}"
+
+    for f in root:
+        fpath = Path(str(f.find(f"{ns}foN").text).strip())  # type: ignore
+        fparts = fpath.parts
+        if len(fparts) < 3:
+            # print(f"{fpath}")
+            continue
+        if fparts[-3] != "Documents":
+            continue
+
+        fname = str(f.find(f"{ns}fiN").text).strip()  # type: ignore
+        path = f"{fpath.parts[-2]}/{fpath.parts[-1]}/{fname}"
+
+        if path in checksums:
+            print(f"Path already in 'checksums'-dict: {path}", flush=True)
+            continue
+
+        checksums[path] = str(f.find(f"{ns}md5").text).strip()  # type: ignore
+
+    return checksums
+
+
+def parse_docIndex_xml(xml_file: Path) -> dict[str, list[Path]]:
+    """Returns a dictionary containing suffixes as keys and lists
+    of relative paths as values.
+
+    Params:
+        xml_file: Path to a valid and wellformed docIndex.xml.
 
     Returns:
         A dict with unique lowercase file-suffixes extracted from the
-          original filenames and a list of paths to example files.
+          original filenames and a list of paths to the files.
     """
     # resulting output-dict of suffix: list of filepaths
     files_to_copy: dict[str, list[Path]] = {}
@@ -74,8 +108,6 @@ def parse_docIndex_xml(
         # ensure key exists and is not fully populated, in output-dict
         if suffix not in files_to_copy:
             files_to_copy[suffix] = []
-        elif len(files_to_copy[suffix]) >= max_examples:
-            continue
 
         # assemble the relative path to the document that we have to copy
         # e.g. 'docCollection2/32/1.tif' (dCf / dID / "1." + aFt)
@@ -92,80 +124,133 @@ def parse_docIndex_xml(
 
 
 def copy_files(
-    files: dict[str, list[Path]], document_dir: Path, output_dir: Path
+    files: dict[str, list[Path]],
+    checksums: dict[str, str],
+    document_dir: Path,
+    output_dir: Path,
+    max_examples: int,
 ) -> None:
     """Copies a dict of suffix: list of filepath to a given output_dir
 
     Params:
-        files:
+        files: Dict of suffix: filepaths[]
+        checksums: Dict of filepath: MD5 checksum
+        doc_dir: Path to the 'Documents' folder of the AVID
         output_dir: Path to the destination directory
+        max_examples: Maximum number of examples files returned
 
     Returns:
         None
     """
+    copied_files: dict[str, list[str]] = {}
+
     for suffix, file_paths in files.items():
-        if suffix in ILLIGAL_NAMES:
+        # rename if suffix is illegal
+        if suffix in ILLEGAL_NAMES:
             suffix = suffix + " (windows renaming)"
 
-        folder_path = Path(output_dir, suffix)
-        try:
-            folder_path.mkdir(parents=True, exist_ok=False)
-        except Exception as e:
-            exit(f"Unable to create output-path: {folder_path}: {e}")
+        # ensure suffix is in copied_files
+        if suffix not in copied_files:
+            copied_files[suffix] = []
 
-        # for each "{docCollection}/{id}/1.{ext}" copy to output_dir / suffix
+        # ensure suffix is in output_dir
+        suffix_path = Path(output_dir, suffix)
+        if not suffix_path.exists():
+            suffix_path.mkdir(parents=True)
+
+        # for each "{docCollection}/{id}/1.{ext}" copy to output_dir/suffix
         for file_path in file_paths:
-            fname = str(file_path).replace("\\", "_")
+            if len(copied_files[suffix]) == max_examples:
+                print(f"Reached limit of {max_examples} for ext: {suffix}")
+                break
+
+            fname = "/".join(file_path.parts)
+            checksum = checksums.get(fname, "")
+            # if checksum already copied
+            if checksum in copied_files[suffix]:
+                print(f"Checksum already copied: {checksum}")
+                continue
+
+            source_path = Path(document_dir, file_path)
+            if not source_path.exists():
+                print(f"File from docIndex not found: {source_path}")
+                continue
+
             try:
-                source = Path(document_dir, file_path)
-                shutil.copy2(source, folder_path / fname)
+                shutil.copy2(
+                    source_path,
+                    Path(output_dir / suffix / fname.replace("/", "_")),
+                )
+                copied_files[suffix].append(checksum)
             except Exception as e:
-                print(f"Unable to copy file to: {folder_path / fname} {e}")
+                print(f"Error copying file: {source_path} {e}")
 
 
 def main(args=None):
     parser = argparse.ArgumentParser(
         description=(
-            "Takes the first 3 files with the same suffix and copy them to"
-            " output directory."
-        )
+            "Copies examples of statutory files from an AVID-compliant"
+            " Documents' folder to the supplied output_dir.\n\n"
+            "The statutory files to copy are grouped by the suffix of their"
+            " original filename, found in the '<oFn>' tag in 'docIndex.xml'"
+        ),
+        formatter_class=argparse.RawDescriptionHelpFormatter,
     )
     parser.add_argument(
-        "input", metavar="AVID_dir", type=str, help="Input AVID directory."
+        "input",
+        metavar="avid_dir",
+        type=Path,
+        help="Path to the root of a valid AVID-package",
     )
     parser.add_argument(
         "output",
         metavar="output_dir",
         type=Path,
-        help="Output directory for copied files.",
+        help="Output directory for copied files",
     )
     parser.add_argument(
-        "--max_ex",
-        metavar="max_examples",
+        "--max",
         type=int,
-        help="Sets the number of copied examples.",
+        default=5,
+        help=(
+            "max number of example files to copy for each file"
+            " extension (default: %(default)s)"
+        ),
     )
-
     parser.add_argument("--version", action="version", version=get_version())
+    parser.add_argument(
+        "--histogram",
+        action="store_true",
+        help=("print a list of suffixes and number of original files"),
+    )
 
     args = parser.parse_args(args)
 
     input_dir = Path(args.input)
     output_dir = Path(args.output)
-    max_ex = args.max_ex or 3
+    max_examples = int(args.max)
 
-    if max_ex < 1:
-        exit("No files was chosen to be copied with --max_ex < 1...exited.")
+    ##############
+    # Validation #
+    ##############
+    if max_examples < 1:
+        exit(
+            "Choose a positive number of example files with the '--max' option"
+        )
 
     if not input_dir.exists():
         exit("Input directory doesn't exists.")
 
-    if not Path(input_dir, "Indices", "docIndex.xml").exists():
-        exit("The given path doesn't contain Indices\\docIndex.xml")
+    fileIndex_xml = Path(input_dir, "Indices", "fileIndex.xml")
+    if not fileIndex_xml.exists():
+        exit("The avid_dir path doesn't contain 'Indices\\fileIndex.xml'")
 
-    # test write access:
+    docIndex_xml = Path(input_dir, "Indices", "docIndex.xml")
+    if not docIndex_xml.exists():
+        exit("The avid_dir path doesn't contain 'Indices\\docIndex.xml'")
+
+    # try to create output_dir
     if not output_dir.exists():
-
         try:
             output_dir.mkdir(parents=True, exist_ok=False)
         except Exception as e:
@@ -173,25 +258,43 @@ def main(args=None):
                 f"Access error: Unable to make directory "
                 f"at the specified location: {e}"
             )
-
+    # try to write a file to output_dir
     try:
         temp_file_path: Path = output_dir / "temp.tmp"
         f = open(temp_file_path, "w")
         f.write("Write access test...")
         f.close()
         temp_file_path.unlink()
-        os.rmdir(Path(output_dir))
-
     except Warning as w:
         exit(
             f"Access error: Unable to create file "
             f"at the specified location: {w}"
         )
 
-    files = parse_docIndex_xml(
-        Path(input_dir, "Indices", "docIndex.xml"), max_ex
+    #############
+    # Histogram #
+    #############
+    if args.histogram:
+        docIndex_files = parse_docIndex_xml(docIndex_xml)
+        print("File extension\tCount", flush=True)
+        for tuple in sorted(
+            docIndex_files.items(), key=lambda i: len(i[1]), reverse=True
+        ):
+            print(f"{tuple[0]}\t\t{len(tuple[1])}", flush=True)
+        exit()
+
+    ##############
+    # copy files #
+    ##############
+    checksums = parse_fileIndex_xml(fileIndex_xml)
+    docIndex_files = parse_docIndex_xml(docIndex_xml)
+    copy_files(
+        docIndex_files,
+        checksums,
+        Path(input_dir, "Documents"),
+        output_dir,
+        max_examples,
     )
-    copy_files(files, Path(input_dir, "Documents"), output_dir)
 
 
 if __name__ == "__main__":
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,34 +1,32 @@
-from statutory_qa import main
-
-# import sys
-# sys.path.append("C:\\Users\\az68636\\github\\statutory-qa\\statutory_qa")
-# import main
 import os
 from pathlib import Path
 
+from statutory_qa import main
+
 
 def test_parse_docIndex_xml():
-
-    xml_file: Path = Path("tests\\docIndex.xml")
-
-    output: dict[str, list[Path]] = main.parse_docIndex_xml(xml_file, 1)
-
+    xml_file = Path("tests\\Indices\\docIndex.xml")
+    output = main.parse_docIndex_xml(xml_file)
     assert output["odt"][0] == Path("docCollection1/1/1.tif")
 
 
 def test_copy_files():
-
     files: dict[str, list[Path]] = {}
     files["odt"] = []
     files["odt"].append(Path("docCollection1/1/1.tif"))
-    # dir_path = Path(os.path.dirname(os.path.realpath(__file__)))
     output_dir = Path(Path(__file__).parent.resolve(), "test_output")
     print(output_dir)
 
     input_dir = Path(__file__).parent.resolve()
     print(input_dir)
-
-    main.copy_files(files, Path(input_dir, "Documents"), output_dir)
+    checksums = main.parse_fileIndex_xml(Path("tests\\Indices\\fileIndex.xml"))
+    main.copy_files(
+        files,
+        checksums,
+        Path(input_dir, "Documents"),
+        output_dir,
+        max_examples=1,
+    )
 
     file = Path(
         Path(__file__).parent.resolve(),
